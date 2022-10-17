cae85d026e28384e750468997b293340d8c36be7
alemba-21 <alembakiragu@gmail.com>
2022-10-17 15:11:15 +0300

    commiting some changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   #we send the GET request to the controller in the index action
   #the GET request is the root path i.e GET /about
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
   get "about", to:"about#index"
   root to: "main#index"
+
 end
